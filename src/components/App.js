8ee620f2ca472c8c51a7ff52c2e628b29a28e462
Luiz Alberto <luiz.trisoft@gmail.com>
2018-05-29 23:22:20 -0300

    Front consumindo o webservice

--- src/components/App.js
+++ src/components/App.js
@@ -5,28 +5,54 @@ export default class App extends React.Component{
 
     constructor(){
         super();
-        this.state = {};
+        // this.state = {};
+        this.state = {
+            produtos:[]
+        };
     }
-
+    
     componentWillMount(){
         // Called the first time the component is loaded right before the component is added to the page
-        this.search();
+        this.search2();
     }
 
     search(){
-        var url = 'http://localhost:3000/list-all-broadband';
+        var url = 'http://localhost:8080/list-all-broadband';
         Request.get(url).then((response) => {
             console.log('Mark I', response);
-          this.setState({
-            
-          });
+            // this.setState({      });
         });
+    }
+
+    search2(){
+        fetch('http://localhost:8080/list-all-broadband')
+        .then(response => response.json())
+        .then(response => {
+            this.setState({
+                produtos: response
+            })
+        }).catch(error =>{
+            console.log(error)
+        })
     }
 
     render(){
         return (
-            <div>
-                <h1>HelloWorld</h1>                
+            <div >
+               
+                <h1>Lista de produtos</h1>                
+                 {this.state.produtos.length}
+                {
+                    this.state.produtos.map(function(p, index){
+                    return(
+                        <tr>
+                        <td>{p.Name}</td>
+                        <td>{p.Price}</td>
+                        <td>{p.Type}</td>
+                        </tr>
+                    );
+                })
+                }
             </div>
         )
     }
